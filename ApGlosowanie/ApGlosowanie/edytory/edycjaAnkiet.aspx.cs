9ca94fc68a338a0ba51bfdba41bb8be870a2ab8f
vengz <dercoti@gmail.com>
2016-06-07 23:22:29 +0200

    najnowsza wersja

---
+++ ApGlosowanie/ApGlosowanie/edytory/edycjaAnkiet.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ApGlosowanie.edytor
+{
+    public partial class edycjaAnkiet : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            //String data = (this.gv_ankiety.FindControl("awwdw") as TextBox).ToString();
+            //tb_data_zakonczenia
+            //String data = (this.gv_ankiety.FindControl("tb_data_zakonczenia") as TextBox).ToString();
+            //String data = (this.gv_ankiety.FindControl("Calendar1") as Calendar).ToString();//.SelectedDate.ToString();
+            int a = 2;
+
+            /*
+                             <asp:Parameter Name="Nazwa" Type="String" />
+                <asp:Parameter Name="Stan" Type="Int32" />
+                <asp:Parameter Name="DataZakonczenia" Type="DateTime" /> 
+             */
+
+            //e.OldValues["DataZakonczenia"] = "1999-01-02 00:00:00";
+            Calendar cal;
+            
+
+            //this.sql_ds_ankiety.UpdateParameters["DataZakonczenia"].DefaultValue = "1999-01-02 00:00:00";
+            
+        }
+
+        protected void sql_ds_ankiety_Updating(object sender, SqlDataSourceCommandEventArgs e)
+        {
+            
+            int a = 2;
+            //e.Cancel = true;
+            //String yolo = this.sql_ds_ankiety.UpdateParameters["DataZakonczenia"].DefaultValue;
+            //this.sql_ds_ankiety.UpdateParameters["DataZakonczenia"].DefaultValue = "1999-01-02 00:00:00";
+        }
+
+        protected void gv_ankiety_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int a = 2;
+        }
+    }
+}
---
+++ ApGlosowanie/ApGlosowanie/edytory/edytorKont.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="edytorKont.aspx.cs" Inherits="ApGlosowanie.edytor.edytorKont" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Table id="tb_dodawanie_wiersza" runat="server">
+            <asp:TableRow>
+                <asp:TableCell>
+                    <asp:Label ID="lb_login" runat="server" Text="Login:"></asp:Label>  
+                </asp:TableCell>
+                <asp:TableCell>
+                    <asp:TextBox ID="tb_login" runat="server"></asp:TextBox>
+                </asp:TableCell>
+            </asp:TableRow>
+            <asp:TableRow>
+                <asp:TableCell>
+                    <asp:Label ID="lb_haslo" runat="server" Text="Hasło:"></asp:Label>
+                </asp:TableCell>
+                <asp:TableCell>
+                    <asp:TextBox ID="tb_haslo" runat="server"></asp:TextBox>    
+                </asp:TableCell>
+            </asp:TableRow>
+            <asp:TableRow>
+                <asp:TableCell>
+                    <asp:Button ID="bt_dodaj" runat="server" Text="Dodaj konto" />
+                </asp:TableCell>
+            </asp:TableRow>
+        </asp:Table>
+  
+        <asp:Table id="Table1" runat="server">
+
+        </asp:Table>
+
+
+    </div>
+    </form>
+</body>
+</html>
